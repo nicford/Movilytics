ecc114aa95f8a274a78a8b85dea0a509839a7b1f
Nicolas Ford <univnf@gmail.com>
2021-03-07 17:31:11 +0100

    added database config from env file

--- group8/apps/api/src/app/database/database.module.ts
+++ group8/apps/api/src/app/database/database.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  providers: [DatabaseService]
+  imports: [ConfigModule.forRoot({
+    envFilePath: "./env/database.env"
+  })],
+  providers: [DatabaseService],
+  exports: [DatabaseService]
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+}
--- group8/apps/api/src/app/database/database.service.ts
+++ group8/apps/api/src/app/database/database.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Pool } from 'pg';
+import { ConfigService } from '@nestjs/config';
+import { Tracing } from 'trace_events';
+
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService {
+
+    postgres_config = {
+        user: this.configService.get<string>('POSTGRES_USER'),
+        host: this.configService.get<string>('POSTGRES_HOST'),
+        database: this.configService.get<string>('POSTGRES_DB'),
+        password: this.configService.get<string>('POSTGRES_PASSWORD'),
+        port: this.configService.get<number>('POSTGRES_PORT'),
+        test: process.env.POSTGRES_PORT
+    }
+
+
+    constructor(private configService: ConfigService) {}
+
+    runQuery(query: string): string {
+        console.log(query);
+        console.log(this.postgres_config);
+        return "Success";
+    }
+
+
+}
