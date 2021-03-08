463d27b4e232fefa268cbb67c15f70cb6dcbf2ba
Nicolas Ford <univnf@gmail.com>
2021-03-08 19:45:00 +0100

    added serving of static files

--- group8/apps/api/src/app/app.module.ts
+++ group8/apps/api/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { MoviesModule } from './movies/movies.module';
 import { GlobalCacheModule } from './global-cache/global-cache.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 @Module({
-  imports: [DatabaseModule, MoviesModule, GlobalCacheModule],
+  imports: [DatabaseModule, MoviesModule, GlobalCacheModule, ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '../../../dist/apps/ucl-imdb')
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
