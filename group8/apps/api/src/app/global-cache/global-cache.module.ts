a9e595da7b86a896bfdf954033cdf53fc8218719
Nicolas Ford <univnf@gmail.com>
2021-03-10 20:34:10 +0100

    integrated development and production redis cache settings

--- group8/apps/api/src/app/global-cache/global-cache.module.ts
+++ group8/apps/api/src/app/global-cache/global-cache.module.ts
@@ -2,15 +2,18 @@ import { CacheInterceptor, CacheModule, Global, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import * as redisStore from 'cache-manager-redis-store';
 
+
+
+
 @Global()
 @Module({
     imports: [CacheModule.register({
-            store: redisStore,
-            host: 'redis',
-            port: 6379,
-            ttl: 5,
-            max: null,
-        })],
+        store: redisStore,
+        host: process.env.REDIS_HOST || 'redis',
+        port: Number(process.env.REDIS_PORT) || 6379,
+        ttl: Number(process.env.CACHE_TIMEOUT) || 5,
+        max: Number(process.env.CACHE_MAX) || null,
+    })],
     providers: [{
         // make cache globally available
         provide: APP_INTERCEPTOR,
