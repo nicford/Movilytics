bd33c464ef45052de0ad2e718873d83449782970
Nicolas Ford <univnf@gmail.com>
2021-03-10 03:17:40 +0100

    imported file so that it would be included in transpiled production code too

--- group8/apps/api/src/main.ts
+++ group8/apps/api/src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
