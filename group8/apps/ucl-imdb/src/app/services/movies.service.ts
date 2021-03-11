0701a6d9d0d8419e563df1dd9ea050b79ba78f06
Nicolas Ford <univnf@gmail.com>
2021-03-11 21:45:53 +0100

    created movie service

---
+++ group8/apps/ucl-imdb/src/app/services/movies.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  // constructor() {}
+
+  // createMovieObj(resData: string) {}
+}
