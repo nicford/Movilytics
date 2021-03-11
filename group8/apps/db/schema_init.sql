dc666e0b3c4f1f32293011471cfeb323cca59925
SteveLim99 <stevelimzilian@gmail.com>
2021-03-12 02:55:10 +0800

    new data init and schema init scripts

--- group8/apps/db/schema_init.sql
+++ group8/apps/db/schema_init.sql
@@ -5,8 +5,6 @@
 -- Dumped from database version 13.2 (Debian 13.2-1.pgdg100+1)
 -- Dumped by pg_dump version 13.2
 
--- Started on 2021-03-10 19:09:12 UTC
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -23,7 +21,6 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 200 (class 1259 OID 16385)
 -- Name: companies; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -37,7 +34,6 @@ CREATE TABLE public.companies (
 ALTER TABLE public.companies OWNER TO postgres;
 
 --
--- TOC entry 201 (class 1259 OID 16388)
 -- Name: companies_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -52,7 +48,6 @@ ALTER TABLE public.companies ALTER COLUMN index ADD GENERATED ALWAYS AS IDENTITY
 
 
 --
--- TOC entry 202 (class 1259 OID 16390)
 -- Name: company_info; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -66,7 +61,6 @@ CREATE TABLE public.company_info (
 ALTER TABLE public.company_info OWNER TO postgres;
 
 --
--- TOC entry 203 (class 1259 OID 16393)
 -- Name: countries; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -80,7 +74,6 @@ CREATE TABLE public.countries (
 ALTER TABLE public.countries OWNER TO postgres;
 
 --
--- TOC entry 204 (class 1259 OID 16396)
 -- Name: countries_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -95,7 +88,6 @@ ALTER TABLE public.countries ALTER COLUMN index ADD GENERATED ALWAYS AS IDENTITY
 
 
 --
--- TOC entry 205 (class 1259 OID 16398)
 -- Name: country_info; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -108,7 +100,6 @@ CREATE TABLE public.country_info (
 ALTER TABLE public.country_info OWNER TO postgres;
 
 --
--- TOC entry 206 (class 1259 OID 16401)
 -- Name: genre_info; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -121,7 +112,6 @@ CREATE TABLE public.genre_info (
 ALTER TABLE public.genre_info OWNER TO postgres;
 
 --
--- TOC entry 207 (class 1259 OID 16404)
 -- Name: genres; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -135,7 +125,6 @@ CREATE TABLE public.genres (
 ALTER TABLE public.genres OWNER TO postgres;
 
 --
--- TOC entry 208 (class 1259 OID 16407)
 -- Name: genres_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -150,7 +139,6 @@ ALTER TABLE public.genres ALTER COLUMN index ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 209 (class 1259 OID 16409)
 -- Name: language_info; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -163,7 +151,6 @@ CREATE TABLE public.language_info (
 ALTER TABLE public.language_info OWNER TO postgres;
 
 --
--- TOC entry 210 (class 1259 OID 16412)
 -- Name: movies; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -188,7 +175,6 @@ CREATE TABLE public.movies (
 ALTER TABLE public.movies OWNER TO postgres;
 
 --
--- TOC entry 211 (class 1259 OID 16418)
 -- Name: people; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -202,7 +188,6 @@ CREATE TABLE public.people (
 ALTER TABLE public.people OWNER TO postgres;
 
 --
--- TOC entry 212 (class 1259 OID 16421)
 -- Name: people_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -217,7 +202,6 @@ ALTER TABLE public.people ALTER COLUMN index ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 213 (class 1259 OID 16423)
 -- Name: person; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -235,7 +219,6 @@ CREATE TABLE public.person (
 ALTER TABLE public.person OWNER TO postgres;
 
 --
--- TOC entry 214 (class 1259 OID 16426)
 -- Name: ratings; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -251,7 +234,6 @@ CREATE TABLE public.ratings (
 ALTER TABLE public.ratings OWNER TO postgres;
 
 --
--- TOC entry 215 (class 1259 OID 16429)
 -- Name: ratings_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -266,7 +248,6 @@ ALTER TABLE public.ratings ALTER COLUMN index ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 216 (class 1259 OID 16431)
 -- Name: spoken_languages; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -280,7 +261,6 @@ CREATE TABLE public.spoken_languages (
 ALTER TABLE public.spoken_languages OWNER TO postgres;
 
 --
--- TOC entry 217 (class 1259 OID 16434)
 -- Name: spoken_languages_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -295,7 +275,6 @@ ALTER TABLE public.spoken_languages ALTER COLUMN index ADD GENERATED ALWAYS AS I
 
 
 --
--- TOC entry 220 (class 1259 OID 16565)
 -- Name: tags; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -311,7 +290,6 @@ CREATE TABLE public.tags (
 ALTER TABLE public.tags OWNER TO postgres;
 
 --
--- TOC entry 219 (class 1259 OID 16563)
 -- Name: tags_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -326,7 +304,33 @@ ALTER TABLE public.tags ALTER COLUMN index ADD GENERATED ALWAYS AS IDENTITY (
 
 
 --
--- TOC entry 218 (class 1259 OID 16441)
+-- Name: translations; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.translations (
+    index integer NOT NULL,
+    mid integer NOT NULL,
+    language_id character varying(2) NOT NULL
+);
+
+
+ALTER TABLE public.translations OWNER TO postgres;
+
+--
+-- Name: translations_index_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.translations ALTER COLUMN index ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.translations_index_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
 -- Name: user_names; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -340,7 +344,6 @@ CREATE TABLE public.user_names (
 ALTER TABLE public.user_names OWNER TO postgres;
 
 --
--- TOC entry 2869 (class 2606 OID 16445)
 -- Name: companies companies_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -349,7 +352,6 @@ ALTER TABLE ONLY public.companies
 
 
 --
--- TOC entry 2871 (class 2606 OID 16447)
 -- Name: company_info company_info_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -358,7 +360,6 @@ ALTER TABLE ONLY public.company_info
 
 
 --
--- TOC entry 2873 (class 2606 OID 16449)
 -- Name: countries countries_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -367,7 +368,6 @@ ALTER TABLE ONLY public.countries
 
 
 --
--- TOC entry 2875 (class 2606 OID 16451)
 -- Name: country_info country_info_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -376,7 +376,6 @@ ALTER TABLE ONLY public.country_info
 
 
 --
--- TOC entry 2877 (class 2606 OID 16453)
 -- Name: genre_info genre_info_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -385,7 +384,6 @@ ALTER TABLE ONLY public.genre_info
 
 
 --
--- TOC entry 2879 (class 2606 OID 16455)
 -- Name: genres genres_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -394,7 +392,6 @@ ALTER TABLE ONLY public.genres
 
 
 --
--- TOC entry 2881 (class 2606 OID 16457)
 -- Name: language_info language_info_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -403,7 +400,6 @@ ALTER TABLE ONLY public.language_info
 
 
 --
--- TOC entry 2883 (class 2606 OID 16459)
 -- Name: movies movies_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -412,7 +408,6 @@ ALTER TABLE ONLY public.movies
 
 
 --
--- TOC entry 2885 (class 2606 OID 16461)
 -- Name: people people_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -421,7 +416,6 @@ ALTER TABLE ONLY public.people
 
 
 --
--- TOC entry 2887 (class 2606 OID 16463)
 -- Name: person person_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -430,7 +424,6 @@ ALTER TABLE ONLY public.person
 
 
 --
--- TOC entry 2889 (class 2606 OID 16465)
 -- Name: ratings ratings_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -439,7 +432,6 @@ ALTER TABLE ONLY public.ratings
 
 
 --
--- TOC entry 2891 (class 2606 OID 16467)
 -- Name: spoken_languages spoken_languages_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -448,7 +440,6 @@ ALTER TABLE ONLY public.spoken_languages
 
 
 --
--- TOC entry 2895 (class 2606 OID 16569)
 -- Name: tags tags_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -457,7 +448,14 @@ ALTER TABLE ONLY public.tags
 
 
 --
--- TOC entry 2893 (class 2606 OID 16471)
+-- Name: translations translations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.translations
+    ADD CONSTRAINT translations_pkey PRIMARY KEY (index);
+
+
+--
 -- Name: user_names user_names_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -466,7 +464,6 @@ ALTER TABLE ONLY public.user_names
 
 
 --
--- TOC entry 2896 (class 2606 OID 16472)
 -- Name: companies companies_company_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -475,7 +472,6 @@ ALTER TABLE ONLY public.companies
 
 
 --
--- TOC entry 2897 (class 2606 OID 16477)
 -- Name: companies companies_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -484,7 +480,6 @@ ALTER TABLE ONLY public.companies
 
 
 --
--- TOC entry 2898 (class 2606 OID 16482)
 -- Name: company_info company_info_country_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -493,7 +488,6 @@ ALTER TABLE ONLY public.company_info
 
 
 --
--- TOC entry 2899 (class 2606 OID 16487)
 -- Name: countries countries_country_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -502,7 +496,6 @@ ALTER TABLE ONLY public.countries
 
 
 --
--- TOC entry 2900 (class 2606 OID 16492)
 -- Name: countries countries_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -511,7 +504,6 @@ ALTER TABLE ONLY public.countries
 
 
 --
--- TOC entry 2901 (class 2606 OID 16497)
 -- Name: genres genres_genre_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -520,7 +512,6 @@ ALTER TABLE ONLY public.genres
 
 
 --
--- TOC entry 2902 (class 2606 OID 16502)
 -- Name: genres genres_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -529,7 +520,6 @@ ALTER TABLE ONLY public.genres
 
 
 --
--- TOC entry 2903 (class 2606 OID 16507)
 -- Name: people people_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -538,7 +528,6 @@ ALTER TABLE ONLY public.people
 
 
 --
--- TOC entry 2904 (class 2606 OID 16512)
 -- Name: people people_person_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -547,7 +536,6 @@ ALTER TABLE ONLY public.people
 
 
 --
--- TOC entry 2905 (class 2606 OID 16517)
 -- Name: ratings ratings_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -556,7 +544,6 @@ ALTER TABLE ONLY public.ratings
 
 
 --
--- TOC entry 2906 (class 2606 OID 16522)
 -- Name: ratings ratings_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -565,7 +552,6 @@ ALTER TABLE ONLY public.ratings
 
 
 --
--- TOC entry 2907 (class 2606 OID 16527)
 -- Name: spoken_languages spoken_languages_language_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -574,7 +560,6 @@ ALTER TABLE ONLY public.spoken_languages
 
 
 --
--- TOC entry 2908 (class 2606 OID 16532)
 -- Name: spoken_languages spoken_languages_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -583,7 +568,6 @@ ALTER TABLE ONLY public.spoken_languages
 
 
 --
--- TOC entry 2910 (class 2606 OID 16575)
 -- Name: tags tags_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -592,7 +576,6 @@ ALTER TABLE ONLY public.tags
 
 
 --
--- TOC entry 2909 (class 2606 OID 16570)
 -- Name: tags tags_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -600,7 +583,21 @@ ALTER TABLE ONLY public.tags
     ADD CONSTRAINT tags_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.user_names(user_id);
 
 
--- Completed on 2021-03-10 19:09:17 UTC
+--
+-- Name: translations translations_language_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.translations
+    ADD CONSTRAINT translations_language_id_fkey FOREIGN KEY (language_id) REFERENCES public.language_info(language_id);
+
+
+--
+-- Name: translations translations_mid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.translations
+    ADD CONSTRAINT translations_mid_fkey FOREIGN KEY (mid) REFERENCES public.movies(mid);
+
 
 --
 -- PostgreSQL database dump complete
