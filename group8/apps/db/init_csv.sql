2846a7940f820a5e75c6dcb4ad2a5ed746aba62f
kimdanny <dannykim153@gmail.com>
2021-03-12 20:21:55 +0900

    init_csv.sql

--- group8/apps/db/init_csv.sql
+++ group8/apps/db/init_csv.sql
@@ -4,25 +4,25 @@ DELIMITER ','
 CSV HEADER;
  
  
-COPY people(index,mid,person_id)
+COPY people(mid,person_id)
 FROM '/var/lib/postgresql/data/db_files/people.csv' 
 DELIMITER ',' 
 CSV HEADER;
  
  
-COPY countries(index,mid,country_id)
+COPY countries(mid,country_id)
 FROM '/var/lib/postgresql/data/db_files/countries.csv' 
 DELIMITER ',' 
 CSV HEADER;
  
  
-COPY spoken_languages(index,mid,language_id)
+COPY spoken_languages(mid,language_id)
 FROM '/var/lib/postgresql/data/db_files/spoken_languages.csv' 
 DELIMITER ',' 
 CSV HEADER;
  
  
-COPY rating(index,user_id,mid,rating,timestamp)
+COPY rating(user_id,mid,rating,timestamp)
 FROM '/var/lib/postgresql/data/db_files/rating.csv' 
 DELIMITER ',' 
 CSV HEADER;
@@ -40,7 +40,7 @@ DELIMITER ','
 CSV HEADER;
  
  
-COPY companies(index,mid,company_id)
+COPY companies(mid,company_id)
 FROM '/var/lib/postgresql/data/db_files/companies.csv' 
 DELIMITER ',' 
 CSV HEADER;
@@ -52,13 +52,13 @@ DELIMITER ','
 CSV HEADER;
  
  
-COPY tags(index,user_id,mid,tag,timestamp)
+COPY tags(user_id,mid,tag,timestamp)
 FROM '/var/lib/postgresql/data/db_files/tags.csv' 
 DELIMITER ',' 
 CSV HEADER;
  
  
-COPY genres(index,mid,genre_id)
+COPY genres(mid,genre_id)
 FROM '/var/lib/postgresql/data/db_files/genres.csv' 
 DELIMITER ',' 
 CSV HEADER;
@@ -76,9 +76,13 @@ DELIMITER ','
 CSV HEADER;
  
  
-COPY movies(mid,adult,budget,language_id,overview,popularity,poster_path,runtime,status,tagline,title,video,vote_average,vote_count)
+COPY movies(mid,adult,budget,language_id,overview,popularity,poster_path,runtime,status,tagline,title,video,vote_average,vote_count,year,revenue)
 FROM '/var/lib/postgresql/data/db_files/movies.csv' 
 DELIMITER ',' 
 CSV HEADER;
  
  
+COPY translations(mid,language_id)
+FROM '/var/lib/postgresql/data/db_files/translations.csv' 
+DELIMITER ',' 
+CSV HEADER;
