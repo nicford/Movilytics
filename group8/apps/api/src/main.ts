ffae5aa72aae695dc23d7800dc11b20371f400c2
Nicolas Ford <univnf@gmail.com>
2021-03-13 23:04:34 +0100

    added whitelist to incoming data

--- group8/apps/api/src/main.ts
+++ group8/apps/api/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));   // convert incoming string to expected type
+  app.useGlobalPipes(new ValidationPipe({ transform: true , whitelist: true}));   // convert incoming string to expected type
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
