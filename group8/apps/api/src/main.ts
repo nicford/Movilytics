6fa8c8430f28fd45d551eade9c7bb8af9ed3ef91
Nicolas Ford <univnf@gmail.com>
2021-03-13 02:29:33 +0100

    parameters are now converted to theri dto types

--- group8/apps/api/src/main.ts
+++ group8/apps/api/src/main.ts
@@ -13,8 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true } })
-  )
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));   // convert incoming string to expected type
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
