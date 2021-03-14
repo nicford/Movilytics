f0e6890a5b58e17ffbb59397fa8112325817db8b
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2021-03-15 01:52:22 +0800

    added function to get avg genre and avg company rating

---
+++ group8/apps/db/db_functions/get_genre_company_avg.sql
@@ -0,0 +1,32 @@
+-- function to get average genre rating and company rating 
+create or replace function get_genre_company_avg (
+	movie_id integer DEFAULT NULL
+) 
+	returns table(
+		genre_avg decimal,
+		company_avg decimal 
+	)
+	language plpgsql
+as $$
+	begin 	
+		return query 
+			with 
+				genre_table as (
+					select avg(genre_info.genre_avg_rating) as genre_avg 
+					from genre_info 
+						where genre_info.genre_id in (
+							select genres.genre_id from genres where genres.mid = movie_id
+						)
+				),
+				company_table as (
+					select avg(company_info.avg_company_rating) as company_avg 
+					from company_info 
+						where company_info.company_id  in (
+							select companies.company_id from companies where companies.mid = movie_id
+						)
+				)
+			
+			select genre_table.genre_avg, company_table.company_avg from genre_table, company_table;
+end;$$
+			
+			
