3e9782205a06c86052d347b942a4b4199d99a0c3
Nicolas Ford <univnf@gmail.com>
2021-03-17 03:44:10 +0100

    fixed major caching issue for movies search, and added pre-caching of sort_by and ascending options

--- group8/apps/api/src/app/movies/movies.service.ts
+++ group8/apps/api/src/app/movies/movies.service.ts
@@ -4,7 +4,7 @@ import { DatabaseService } from '../database/database.service';
 import { Cache } from 'cache-manager';
 
 import { exec } from 'child_process';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as dotenv from "dotenv";
 import { searchDto, searchResponse } from '@group8/api-interfaces';
@@ -31,6 +31,14 @@ export class MoviesService {
   tmdb_api_endpoint = 'http://api.themoviedb.org/3';
   api_key_suffix = `api_key=${process.env.TMDB_API_KEY}`;
 
+  sort_by_values = [
+    "popularity",
+    "polarity",
+    "title",
+    "year",
+    "rating"
+  ]
+
   async getMovie(movie_id: number) {
     // console.log(this.cache.store)
     // const query = "SELECT * FROM LINKS WHERE mid = $1";
@@ -45,24 +53,29 @@ export class MoviesService {
   }
 
 
-  // TODO: ignore case when searching!!
-  async search(search_query: searchDto) {
-    const search_query_hash = sha1(search_query)
-    console.log(search_query_hash)
+  // from_cache: true if being called from a cache, false otherwise
+  async search(search_query: searchDto, from_cache=false) {
+    const search_query_hash = sha1(search_query);
+    console.log(`INCOMING SEARCH: hash: ${search_query_hash}`);
 
-    if (await this.cache.get(search_query_hash)) {
-      console.log('found in cache');
+    // Only check if in cache if not coming from cache.
+    // If coming from a cache function, then we already know there won't be a cache hit
+    // if (!from_cache) {
+      console.log('checking cache');
       const cache_hit = await this.cache.get(search_query_hash);
-      console.log(`cache hit: ${cache_hit}`);
-      return cache_hit;
-    }
+      if (cache_hit) {
+        console.log('found in cache');
+        console.log(`cache hit: ${cache_hit}`);
+        return cache_hit;
+      }
+    // }
     
+    // getting parameters and preparing them for SQL query
     const query_prepared_parameters: string[] = [];
     const parameters: (string | number | boolean | number[])[] = [];
     let index = 1;
     let value: string | number | boolean | number[];
     Object.keys(search_query).forEach((key) => {
-      console.log(key);
       value = search_query[key];
       if (typeof value !== 'undefined' && value) {
         query_prepared_parameters.push(`${key} := $${index}`);
@@ -72,35 +85,60 @@ export class MoviesService {
     });
     console.log(query_prepared_parameters);
     console.log(parameters);
-    const sql_query = `select * from get_movies(${query_prepared_parameters.join(', ')})`;
+    const sql_query = `SELECT * FROM get_movies(${query_prepared_parameters.join(', ')})`;
     console.log(sql_query)
     const response = (await this.databaseService.runQuery(sql_query, parameters)).rows; // convert each parameter to a string before querying the postgres database
 
     console.log(`adding to cache with key (hash): ${search_query_hash}`)
     this.cache.set(search_query_hash, response);
 
-    // cache extra page incase of more requests
-    this.cache_extra_pages(search_query, sql_query, parameters, 4);
+    // only cache if coming from a controller, otherwise the recursion will never end
+    if (!from_cache) {
+      // asynchronously pre-cache some expected search based on the current search
+      this.pre_cache_searches(search_query, sql_query, parameters, 5);
+    }
 
-    // check if first n pages are in cache. get pages
-    // this.cache.set() // query extra pages and add them to cache. Use hash of request parameters and page number for that request as key 
+    console.log();
+    console.log();
+
     return response;
+  }
+
+  async pre_cache_searches(search_query: searchDto, sql_query: string, parameters: any[], number_extra_pages_to_cache: number) {
+      await this.cache_extra_pages(search_query, sql_query, parameters, 4);
+      await this.cache_sortBy_and_order(search_query, sql_query, parameters);
   }
 
 
   // cache extra pages. when calling this function, don't call await so that this can be done in the background without blocking execution
   async cache_extra_pages(search_query: searchDto, sql_query: string, parameters: any[], number_extra_pages_to_cache: number) {
-    console.log('cache extra pages')
+    console.log('CACHING EXTRA PAGES!')
     // go through each page and cache them
+    const result_limit = search_query['result_limit']
+    console.log(`result limit: ${result_limit}`)
+    console.log(`offset: ${search_query['result_offset']}`)
     number_extra_pages_to_cache++; // increase by one to start at offset one
     for (let i = 1; i < number_extra_pages_to_cache; i++) {
-      search_query['result_offset'] = i*search_query['result_limit'];
-      console.log(`query: ${search_query['result_offset']}`)
-      const response = (await this.databaseService.runQuery(sql_query, parameters)).rows; 
-      console.log(`caching ${sha1(search_query)} with value ${response}`)
-      this.cache.set(sha1(search_query), response);
+      search_query['result_offset'] += result_limit;
+      console.log(`caching offset: ${search_query['result_offset']}`)
+      await this.search(search_query, true);  // values get cached during search. Await, because same search_query object is being used, and can't change while called functions runs
     }
-    
+  }
+
+  // cache all sort by options for search query, including ascending/descending ordering for all sort options!
+  async cache_sortBy_and_order(search_query: searchDto, sql_query: string, parameters: any[]) {
+    console.log('CACHING SORT BY VALUES');
+    const current_sortBy_value = search_query['sort_by'];
+    for (const sortBy_option of this.sort_by_values) {
+      if (current_sortBy_value != sortBy_option) {
+        for (const order_option of ["true", "false"]) {
+          search_query['sort_by'] = sortBy_option;
+          search_query['ascending'] = order_option;
+          console.log(`Caching options: ${sortBy_option}, ascending: ${order_option}`);
+          await this.search(search_query, true);  // values get cached during search. Await, because same search_query object is being used, and can't change while called functions runs
+        }
+      }
+    }
   }
 
   
