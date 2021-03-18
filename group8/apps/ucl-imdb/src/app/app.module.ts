d5855a0aee09bebcf0929eb70004e83a4191f403
kimdanny <dannykim153@gmail.com>
2021-03-19 06:35:20 +0900

    Chart.js Set up - check TODO in app.module.ts

--- group8/apps/ucl-imdb/src/app/app.module.ts
+++ group8/apps/ucl-imdb/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterModule, Routes } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import {HttpClientModule} from '@angular/common/http';
@@ -8,11 +8,20 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReviewComponent } from './review/review.component';
+import {ChartsModule} from 'ng2-charts';
+
+// TODO: Get mid from movies.service.ts
+const routes: Routes = [
+  {path: 'tabs/tab2/299536', component: ReviewComponent}
+]
 
 @NgModule({
   declarations: [AppComponent, ReviewComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), 
+            AppRoutingModule, HttpClientModule, ChartsModule,
+            RouterModule.forRoot(routes)
+            ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- group8/apps/ucl-imdb/src/app/review/review.component.ts
+++ group8/apps/ucl-imdb/src/app/review/review.component.ts
@@ -4,6 +4,7 @@ import { ImagesService } from '../services/images.service';
 import { MoviesService } from '../services/movies.service';
 import { movieRes } from './movieRes.interface';
 
+
 @Component({
   selector: 'group8-review',
   templateUrl: './review.component.html',
@@ -25,6 +26,39 @@ export class ReviewComponent implements OnInit {
       this.poster = this.getImage(this.movie.poster_path)
     }).unsubscribe
   }
+
+  // Bart Chart
+  public barChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+  public barChartData = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+  // Pie Chart
+  public pieChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public pieChartData = [120, 150, 180, 90];
+  public pieChartType = 'pie';
+  
+  // Doughnut Chart
+  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public doughnutChartData = [120, 150, 180, 90];
+  public doughnutChartType = 'doughnut';
+
+  // Radar Chart
+  public radarChartLabels = ['Q1', 'Q2', 'Q3', 'Q4'];
+  public radarChartData = [
+    {data: [120, 130, 180, 70], label: '2017'},
+    {data: [90, 150, 200, 45], label: '2018'}
+  ];
+  public radarChartType = 'radar';
+
 
   ngOnInit(): void {
   }
