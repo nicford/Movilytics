6f6792d944df322dde5be1c92ce7c848547bae28
SteveLim99 <43622493+SteveLim99@users.noreply.github.com>
2021-03-22 06:13:49 +0800

    query optimization

--- group8/apps/db/prediction_queries/get_personality.sql
+++ group8/apps/db/prediction_queries/get_personality.sql
@@ -20,35 +20,45 @@ as $$
 				where mid in 
 				(
 					select 
-				 		distinct on (mid) mid 
+				 		mid 
 				 	from tags 
 				 	where tag = any(tag_inputs)
+					group by mid
 				)
 			), 
-			sto as (
+			user_rating_map as (
 				select 
 					user_id, 
 					prediction, 
-					t.mid, 
-					t.avg_rating 
+					mov_ratings.mid, 
+					mov_ratings.avg_rating 
 				from user_predictive_rate 
 				inner join (
-					select * from avg_movie_ratings
-				) as t
-				on user_predictive_rate.mid = t.mid
+					select 
+						avg_movie_ratings.mid, 
+						avg_movie_ratings.avg_rating from avg_movie_ratings
+				) as mov_ratings
+				on user_predictive_rate.mid = mov_ratings.mid
 			)
 	
 		select 
-			avg(openness) as openness,
-			avg(agreeableness) as agreeableness,
-			avg(emotional_stability) as emotional_stability,
-			avg(conscientiousness) as conscientiousness,
-			avg(extraversion) as extraversion
-		from sto 
+			COALESCE(avg(traits.openness), 0) as openness,
+			COALESCE(avg(traits.agreeableness), 0) as agreeableness,
+			COALESCE(avg(traits.emotional_stability), 0) as emotional_stability,
+			COALESCE(avg(traits.conscientiousness), 0) as conscientiousness,
+			COALESCE(avg(traits.extraversion), 0) as extraversion
+		from user_rating_map 
 		inner join(
-			select * from user_traits
-		) as j
-		on sto.user_id = j.user_id
-		where prediction > avg_rating;
+			select 
+				user_traits.user_id, 
+				user_traits.openness, 
+				user_traits.agreeableness, 
+				user_traits.emotional_stability, 
+				user_traits.conscientiousness, 
+				user_traits.extraversion 
+			from user_traits
+		) as traits
+		on user_rating_map.user_id = traits.user_id
+		where user_rating_map.prediction > user_rating_map.avg_rating;
 		
 end; $$
